ed315c994d548e0966d0db01410b9125b02db456
adhfmz7 <ahmadaldasouqi559@gmail.com>
2022-05-22 13:53:32 -0700

    commit

--- panda.py
+++ panda.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.utils import ChromeType
+import time
 
 
 code = input("Enter 22-digit code: ")
@@ -11,6 +12,20 @@ global driver
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
 driver.get('https://www.pandaguestexperience.com/')
-
 driver.find_element_by_id('CN1').send_keys(code)
+driver.find_element_by_id('NextButton').click()
+time.sleep(5)
+
+driver.find_element_by_id('R000002.5').click()
+driver.find_element_by_id('NextButton').click()
+time.sleep(5) 
+
+driver.find_element_by_id('R000008.5').click()
+driver.find_element_by_id('R000018.5').click()
+driver.find_element_by_id('R000013.5').click()
+driver.find_element_by_id('R000012.5').click()
+driver.find_element_by_id('R000287.5').click()
+driver.find_element_by_id('R000010.5').click()
+driver.find_element_by_id('NextButton').click()
+time.sleep(5)
 
